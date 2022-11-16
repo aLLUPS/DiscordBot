d45c2e4460fac87ccc049a920b6d846e07a9de86
Pawara Siriwardhane <56413657+aLLUPS@users.noreply.github.com>
2022-11-16 16:48:23 +0000

    - Save user's trigger responses on database - Implement Chatbot on/off

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import random
+from replit import db
 
 intent = discord.Intents.default()
 intent.members = True
@@ -22,6 +23,9 @@ starter_responses = ["Cheer up!",
                     "Don't worry!",
                     "You are great!"]
 
+if "responding" not in db.keys():
+  db["responding"] = True
+
 # helper fn to retrive and return quotes form the api
 def get_quote(feature):
   
@@ -35,6 +39,22 @@ def get_quote(feature):
   
   return quote
 
+# helper fn to add responses messages to database
+def update_responses(response_message):
+  if "msgresponses" in db.keys():
+    msgresponses = db["msgresponses"]
+    msgresponses.append(response_message)
+    db["msgresponses"] = msgresponses
+  else:
+    db["msgresponses"] = [response_message]
+
+# helper fn to delete responses messages to database
+def delete_responses(index):
+  msgresponses = db["msgresponses"]
+  if len(msgresponses) > int(index):
+    del msgresponses[int(index)]
+  db["msgresponses"] = msgresponses
+    
 # to ready the bot
 @client.event
 async def on_ready():
@@ -46,13 +66,56 @@ async def on_message(message):
   msg = message.content
   if message.author == client.user:
     return
+
+  # greeting to the user
   if msg.startswith('Hi'):
     await message.channel.send('Hello!')
+
+  # Display inspirational quotes to the user from the API
   if msg.startswith('$inspire'):
     quote = get_quote('happiness')
     await message.channel.send(quote)
-  if any(word in msg for word in unhappy_triggers):
-    await message.channel.send(random.choice(starter_responses))
 
+  # responding to sad triggers from the user
+  if db["responding"]:
+    options = starter_responses
+    if "msgresponses" in db.keys():
+      options = options + list(db["msgresponses"])
+    
+    if any(word in msg for word in unhappy_triggers):
+      await message.channel.send(random.choice(options))
 
+  # to add the responses from the database
+  if msg.startswith("$new"):
+    msgresponses = msg.split('$new ', 1)[1]
+    update_responses(msgresponses)
+    await message.channel.send("New response message added to database")
+
+  # to delete the responses from the database
+  if msg.startswith("$del"):
+    msgresponses = []
+    if "msgresponses" in db.keys():
+      index = msg.split("$del", 1)[1]
+      delete_responses(index)
+      msgresponses = db["msgresponses"]
+    await message.channel.send(msgresponses)
+
+  # to list down the response messages entered by user
+  if msg.startswith("$list"):
+    msgresponses = []
+    if "msgresponses" in db.keys():
+      msgresponses = db["msgresponses"]
+    await message.channel.send(msgresponses)
+    
+  # to control on/off of the chatbot responding
+  if msg.startswith("$chatbot"):
+    value = msg.split("$chatbot ", 1)[1]
+    if value.lower() == "on":
+      db["responding"] = True
+      await message.channel.send("Chatbot responding is on")
+    else:
+      db["responding"] = False
+      await message.channel.send("Chatbot responding is off")
+    
+# to run the bot
 client.run(bot_secret)
